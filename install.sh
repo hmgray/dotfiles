78ae6e28763e062e877ac21bd2993882151892c5
Hope M Gray <hmg@127.0.0.1>
2022-12-12 01:40:23 -0800

    Remove non-standard flag from `mv`.
    The `-v` flag is not part of POSIX, which might cause problems when
    moving between machines while using the same configuration.
    
    See https://pubs.opengroup.org/onlinepubs/9699919799/utilities/mv.html#tag_20_82.

--- install.sh
+++ install.sh
@@ -7,7 +7,8 @@ cd $BASE_DIR
 for i in _*; do
   LINK_NAME="$HOME/"$(echo "$i" | sed s/_/./)""
   if [ -e "$LINK_NAME" ]; then
-    mv --verbose "$LINK_NAME" "$LINK_NAME.bak"
+    mv "$LINK_NAME" "$LINK_NAME.bak"
+    echo "Moved $LINK_NAME to $LINK_NAME.bak."
   fi
   ln -s "$BASE_DIR/$i" "$LINK_NAME"
 done
